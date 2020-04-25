435d9fbe71147f49bdd8bdba07e885a72edbb511
Diganta Som <52863377+DigSmash111@users.noreply.github.com>
2020-04-25 22:16:02 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -14,9 +14,9 @@ export const fetchData = async (country) => {
 		 return response; */
 
 		 // taking only relevant data from the API (destructering the api json)
-		const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);	// url, in the beginning
+		const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);	
 
-		const modifiedData = {confirmed, recovered, deaths, lastUpdate};	// if key and value's names are equal, then we can use js objects like this
+		const modifiedData = {confirmed, recovered, deaths, lastUpdate};	
 		return modifiedData;
 
 	} catch (error) {
@@ -26,7 +26,7 @@ export const fetchData = async (country) => {
 
 export const fetchDailyData = async () => {
 	try {
-		const { data } = await axios.get(`${url}/daily`);	// after destructoring the object, we got array of objects
+		const { data } = await axios.get(`${url}/daily`);	
 		// console.log(data);
 
 		const modifiedData = data.map(dailyData => ({
@@ -44,9 +44,9 @@ export const fetchDailyData = async () => {
 
 export const fetchCountries = async () => {
 	try {
-		const { data: {countries} } = await axios.get(`${url}/countries`);	// we get, array of objects
+		const { data: {countries} } = await axios.get(`${url}/countries`);	
 		
-		return countries.map(country => country.name);	// we don't want iso2, iso3, we want just the name of each country
+		return countries.map(country => country.name);	
 
 	} catch (error) {
 		console.log(error);
